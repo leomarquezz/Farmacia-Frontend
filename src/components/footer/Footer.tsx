cb4b4f8701a2ee5649408d00eb2e7abc33b1c1bc
leomarquezz <89556799+leomarquezz@users.noreply.github.com>
2024-02-29 10:58:44 -0300

    Frontend do Projeto Farmacia

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,17 @@
+
+function Footer() {
+ 
+  
+
+  return (
+    <>
+       <div className="flex justify-center bg-purple-400 text-white">
+          <div className="container flex flex-col items-center py-4">
+            <p className='text-xl font-bold'>Farmacia Generation| Copyright: </p>
+          </div>
+        </div>
+      </>
+  )
+}
+
+export default Footer
